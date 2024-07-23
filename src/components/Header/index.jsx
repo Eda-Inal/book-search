9d225b6dbf960a021715535d45f2114d2acd6fa4
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-23 10:18:44 +0300

    created sass and header

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./index.scss"
+
+function Header() {
+    return (
+        <header className='text-center header' >
+            <h1 className='h1 '>Find Book</h1>
+        </header>
+    )
+}
+export default Header
