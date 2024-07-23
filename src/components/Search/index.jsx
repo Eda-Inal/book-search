ca3616176f9fab7d0689235176048b7030212ee6
Eda Işıl İnal <edainalisil@gmail.com>
2024-07-23 20:09:54 +0300

    get data with axios

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./index.scss"
 import { IoMdSearch } from "react-icons/io";
+import { useDispatch,useSelector } from 'react-redux';
+import { searchInput,fetchData } from '../../redux/booksSlice';
 
 
 function Search() {
+  const dispatch = useDispatch();
+  const [input , setInput] =  useState("");
+  const [toast,setToast] = useState(false);
+  const contents = useSelector((state) => state.books.contents);
+  console.log(contents);
    
-  
-
-    
-
-  
-
-  
-
+  const handleChange =(e) => {
+setInput(e.target.value)
+  }
+  const handleClick =() => {
+    if(input !== ""){
+dispatch(searchInput(input));
+dispatch(fetchData(input));
+setInput("");
+    }else{
+      setToast(true) // sonra toast bildirimi ekle
+    }
+  }
     return (
     <>
     <div className="input-group flex-nowrap  mx-auto mt-4">
-    <input type="text" placeholder='Search book' class="form-control" style={{fontSize:22}}  aria-describedby="addon-wrapping"/>
-    <span className="input-group-text "  id="addon-wrapping "><IoMdSearch  className='icon' /></span>
+     
+    <input type="text" placeholder='Search book' value={input} onChange={handleChange} class="form-control" style={{fontSize:22}}  aria-describedby="addon-wrapping"/>
+    <span className="input-group-text "  id="addon-wrapping "><IoMdSearch onClick={handleClick}  className='icon' /></span>
 
     
  
--- src/redux/booksSlice.jsx
+++ src/redux/booksSlice.jsx
@@ -1,14 +1,45 @@
-import { createSlice } from '@reduxjs/toolkit'
+import { createSlice,createAsyncThunk } from '@reduxjs/toolkit'
+import axios from 'axios'
+
+
+export const fetchData = createAsyncThunk(
+    'books/fetchData',
+    async (bookname) => {
+        const res = await axios('https://www.googleapis.com/books/v1/volumes?q=${bookname}+inauthor`')
+        const data = await res.data
+        return data.items
+    }
+)
+
 
 export const booksSlice = createSlice({
     name:'books',
     initialState:{
-       
+       bookname:"",
+       contents: [],
+  isLoading: false,
+  error: null,
     },
     reducers:{
-
-    }
+searchInput:(state,action)  => {
+state.bookname = action.payload
+}
+    },
+    extraReducers: (builder) => {
+        builder.addCase(fetchData.pending, (state) => {
+          state.isLoading = true;
+        });
+        builder.addCase(fetchData.fulfilled, (state, action) => {
+          state.isLoading = false;
+          state.contents = action.payload;
+        });
+        builder.addCase(fetchData.rejected, (state, action) => {
+          state.isLoading = false;
+          state.error = action.error.message;
+        });
+      },
+  
 
 })
-// export const {functions} = bookSlice.actions;
+ export const {searchInput} = booksSlice.actions
 export default booksSlice.reducer;
